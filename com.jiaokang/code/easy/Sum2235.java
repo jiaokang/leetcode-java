dd04be4ed883c5dbca14a0e1defae7a3e7cb0392
jiaokang <jiaoikang@gmail.com>
2023-04-27 16:18:33 +0800

    '两整数相加'

---
+++ com.jiaokang/code/easy/Sum2235.java
@@ -0,0 +1,34 @@
+package code.easy;
+
+/**
+ * @ Author     ：焦康
+ * @ Date       ：Created in 16:17 2023-04-27
+ * @ Description：
+ */
+public class Sum2235 {
+    /**
+     * 两整数相加
+     * <p>
+     * 给你两个整数 num1 和 num2，返回这两个整数的和。
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：num1 = 12, num2 = 5
+     * 输出：17
+     * 解释：num1 是 12，num2 是 5 ，它们的和是 12 + 5 = 17 ，因此返回 17 。
+     * 示例 2：
+     * <p>
+     * 输入：num1 = -10, num2 = 4
+     * 输出：-6
+     * 解释：num1 + num2 = -6 ，因此返回 -6 。
+     * <p>
+     * <p>
+     * 提示：
+     * <p>
+     * -100 <= num1, num2 <= 100
+     */
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
