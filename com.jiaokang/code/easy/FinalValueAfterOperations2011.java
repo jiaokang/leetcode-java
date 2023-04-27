a0bb1aa0aaabf6dd9c1ffba7e155f7865a41d6cd
jiaokang <jiaoikang@gmail.com>
2023-04-27 16:01:13 +0800

    '基于排列构建数组'

--- com.jiaokang/code/easy/FinalValueAfterOperations2011.java
+++ com.jiaokang/code/easy/FinalValueAfterOperations2011.java
@@ -1,6 +1,73 @@
-package code.easy;/**
+package code.easy;
+
+import com.zayk.asn1.x509.V;
+import org.omg.CORBA.INTERNAL;
+
+/**
  * @ Author     ：焦康
  * @ Date       ：Created in 15:41 2023-04-27
  * @ Description：
- */public class FinalValueAfterOperations2011 {
+ */
+public class FinalValueAfterOperations2011 {
+    /**
+     * 执行操作后的变量值
+     *
+     * 存在一种仅支持 4 种操作和 1 个变量 X 的编程语言：
+     *
+     * ++X 和 X++ 使变量 X 的值 加 1
+     * --X 和 X-- 使变量 X 的值 减 1
+     * 最初，X 的值是 0
+     *
+     * 给你一个字符串数组 operations ，这是由操作组成的一个列表，返回执行所有操作后， X 的 最终值 。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：operations = ["--X","X++","X++"]
+     * 输出：1
+     * 解释：操作按下述步骤执行：
+     * 最初，X = 0
+     * --X：X 减 1 ，X =  0 - 1 = -1
+     * X++：X 加 1 ，X = -1 + 1 =  0
+     * X++：X 加 1 ，X =  0 + 1 =  1
+     * 示例 2：
+     *
+     * 输入：operations = ["++X","++X","X++"]
+     * 输出：3
+     * 解释：操作按下述步骤执行：
+     * 最初，X = 0
+     * ++X：X 加 1 ，X = 0 + 1 = 1
+     * ++X：X 加 1 ，X = 1 + 1 = 2
+     * X++：X 加 1 ，X = 2 + 1 = 3
+     * 示例 3：
+     *
+     * 输入：operations = ["X++","++X","--X","X--"]
+     * 输出：0
+     * 解释：操作按下述步骤执行：
+     * 最初，X = 0
+     * X++：X 加 1 ，X = 0 + 1 = 1
+     * ++X：X 加 1 ，X = 1 + 1 = 2
+     * --X：X 减 1 ，X = 2 - 1 = 1
+     * X--：X 减 1 ，X = 1 - 1 = 0
+     *
+     *
+     * 提示：
+     *
+     * 1 <= operations.length <= 100
+     * operations[i] 将会是 "++X"、"X++"、"--X" 或 "X--"
+     */
+
+    public int finalValueAfterOperations(String[] operations) {
+        int variable = 0;
+        for (String operation : operations) {
+            if ("++X".equals(operation)||"X++".equals(operation)){
+                variable++;
+            }
+            if ("--X".equals(operation)||"X--".equals(operation)){
+                variable--;
+            }
+        }
+        return variable;
+    }
 }
